97fac9bce5a6b4c2b6508854e9a256b21c971ca6
Joshua Achire <achirejoshua@gmail.com>
2026-08-09 18:38:30 -0400

    Add: sql-top-travellers - submission-0

---
+++ SQL/sql-top-travellers/submission-0.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+SELECT
+    u.name,
+    COALESCE(SUM(r.distance),0) AS travelled_distance
+FROM users AS u
+LEFT JOIN rides AS r
+     ON u.id = r.user_id
+GROUP BY u.name
+ORDER  BY travelled_distance DESC, u.name ASC     
